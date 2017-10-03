22e414778d975cad9c48cf236433b7f2c4031c3b
MidnightDrifter <sean.higgins@digipen.edu>
2017-10-02 18:55:41 -0700

    Core rubix cube functionality set up.  I think!

--- Project4RubixCube/Project4RubixCube/RubixCube.cpp
+++ Project4RubixCube/Project4RubixCube/RubixCube.cpp
@@ -1,12 +1,97 @@
 #include "RubixCube.h"
 
 
+										//Right, left, top, & bottom neighbors--in that order!
+const int RubixCube::neighbors[6][4] = { { 1,3,4,5 }, { 2,0,4,5 }, { 3,1,4,5 }, { 0,2,4,5 }, { 2,0,3,1 }, { 2,0,1,3 } };
 
-RubixCube::RubixCube()
+
+
+
+
+void RubixCube::TurnRow(int row)
 {
+
+	std::vector<int> temp[4];
+	//Based on my drawing, the rows involved in HORIZONTAL MOVES are:  0,1,2,3
+	for (int i = 0; i < 4; i++)
+	{
+		temp[i] = faces[i].myFace[row];
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		faces[i].myFace[(row + 1) % 4] = temp[i];
+	}
+
 }
 
 
-RubixCube::~RubixCube()
+void RubixCube::TurnColumn(int col)
 {
+	std::vector<int> temp[4];
+	//Based on my drawing, the rows involved in VERTICAL MOVES are:  1,4,3,5
+	
+	for (int i = 0; i < 3; i++)
+	{
+		temp[0][i] = faces[1].myFace[i][col];
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		temp[1][i] = faces[4].myFace[i][col];
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		temp[2][i] = faces[3].myFace[i][col];
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		temp[3][i] = faces[5].myFace[i][col];
+	}
+
+
+
+	for (int i = 0; i < 3; i++)
+	{
+		faces[4].myFace[i][col] = temp[0][i];
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		faces[3].myFace[i][col] = temp[1][i];
+	}
+	
+	for (int i = 0; i < 3; i++)
+	{
+		faces[5].myFace[i][col] = temp[2][i];
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		faces[1].myFace[i][col] = temp[3][i];
+	}
+
+
+}
+
+
+void RubixCube::Twist(int rowCol, int index, int numTurns)
+{
+	if (rowCol >= 0)
+	{
+		for (int i = 0; i < numTurns; i++)
+		{
+			TurnRow(index);
+		}
+	}
+
+	else
+	{
+		for (int i = 0; i < numTurns; i++)
+		{
+			TurnColumn(index);
+		}
+	}
 }
--- Project4RubixCube/Project4RubixCube/RubixCube.h
+++ Project4RubixCube/Project4RubixCube/RubixCube.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <random>
 
-enum class COLORS { RED, BLUE, WHITE, GREEN, YELLOW, ORANGE };
+enum class COLORS { RED=0, BLUE, WHITE, GREEN, YELLOW, ORANGE };
 
 
 class RubixCubeFace
@@ -11,12 +11,12 @@ class RubixCubeFace
 	//IsSolved() fn
 	
 public:
-	RubixCubeFace() : myFace(std::vector<std::vector<COLORS>>(3, std::vector<COLORS>(3, COLORS::RED))) {}
-	RubixCubeFace(COLORS c) : myFace(std::vector<std::vector<COLORS>>(3, std::vector<COLORS>(3, c))) {}
+	RubixCubeFace() : myFace(std::vector<std::vector<int>>(3, std::vector<int>(3, (int)COLORS::RED))) {}
+	RubixCubeFace(COLORS c) : myFace(std::vector<std::vector<int>>(3, std::vector<int>(3, (int)c))) {}
 
 	bool isSolved() const {
 
-		COLORS c = myFace[0][0];
+		int c = myFace[0][0];
 
 		for (int i = 0; i < myFace.size(); i++)
 		{
@@ -34,7 +34,7 @@ public:
 
 	~RubixCubeFace() {}
 
-	std::vector<std::vector<COLORS>> myFace;
+	std::vector<std::vector<int>> myFace;
 };
 
 
@@ -51,16 +51,65 @@ public:
 		faces[3] = RubixCubeFace(COLORS::GREEN);
 		faces[4] = RubixCubeFace(COLORS::WHITE);
 		faces[5] = RubixCubeFace(COLORS::ORANGE);
-	
+
+
 	
 	}
-	~RubixCube();
+
+	void Twist(int rowColumn, int index, int numTurns);  //Twist Rubix Cube.   
+	// rowColumn >=0, turn rows,   rowColumn < 0 turn columns
+	//index:  turn the 1st, 2nd, or 3rd row/column
+	//Number of times to turn specified row/column
+
+
+
+	//Note:  Don't need to worry about up vs. down  and  clockwise vs. counterclockwise turns:  three of one will always be equivalent to one of the other
+	//Similar to how 3 left turns make a right turn
+	
+	
+	//Helper methods for Twisting
+	void TurnRow(int row);  //Turn row once
+	void TurnColumn(int col);  //Turn column once
+
+
+
+
+	~RubixCube() {};
 
 	RubixCubeFace faces[6];
+
+	//static std::vector<int> neighbors;
+
+
+	//const static int[][] neighbors =	{1,3,4,5},{0,2,4,5},{1,3,4,5},{0,2,4,5},{0,1,2,3},{0,1,2,3};
+	static const int neighbors[6][4];// = { 1,3,4,5 }, { 0,2,4,5 }, { 1,3,4,5 }, { 0,2,4,5 }, { 0,1,2,3 }, { 0,1,2,3 };
+
+
 
 	//Vector of faces to store all 6 faces of cube--THEN the hard part will be rotating it properly
 	//Number each face, keep track of which faces have which neighbors
 	//Diagram will be very helpful
 
+	/*  Faces 0-5
+
+	Face : Neighbors
+	      [4]
+	       |
+	[0] - [1] - [2] - [3] 
+	       |
+		  [5]
+
+
+	0 : 1,2,4,5
+	1 : 0,2,4,3
+	2 : 1,3,4,5
+	3 : 0,2,4,5
+	4 : 0,1,2,3
+	5 : 0,1,2,3
+	
+	
+	
+	
+	*/
 };
 
