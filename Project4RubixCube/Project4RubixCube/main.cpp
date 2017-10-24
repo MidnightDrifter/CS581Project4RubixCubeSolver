68f5895c35ceffd2618b6a31a7f0662fde4071da
MidnightDrifter <linkmasterchief@aol.com>
2017-10-23 18:41:28 -0700

    Core loop should be done

--- Project4RubixCube/Project4RubixCube/main.cpp
+++ Project4RubixCube/Project4RubixCube/main.cpp
@@ -1,5 +1,6 @@
 #include "RubixCube.h"
 #include <random>
+#include <iostream>
 const int STARTING_TURNS = 5;   //How many times to turn & jumble the Rubix Cube
 
 
@@ -7,23 +8,86 @@ const int STARTING_TURNS = 5;   //How many times to turn & jumble the Rubix Cube
 const int MAX_SIDES_SOLVED = 6;		//Max sides solved:  6
 const int MAX_ROWS_COLS_SOLVED= 18;	//Max rows & cols solved:  3 rows, 3 cols per face, 6 faces -- 3 x 6 = 18
 const int MAX_SQUARES_FILLED=54;	//Max squares filled:  9 squares per face, 6 faces -- 9 x 6 = 54
+const unsigned MAX_ITERATIONS = 100000000;
+const int NUM_ROTATIONS_ROWS = 1;
+const int NUM_ROTATIONS_COLS = 1;
+const int NUM_ROTATIONS_BOTH = 1;
 int main()
 {
 
-	RubixCube startingCube;
+	RubixCube* startingCube = new RubixCube();
 
 	for (int i = 0; i < STARTING_TURNS; i++)
 	{
-		startingCube.Twist(rand() % 2, rand() % 3, rand() % 3 + 1);
+		startingCube->Twist(rand() % 2, rand() % 3, rand() % 3 + 1);
 	}
 
+	unsigned numIterations = 0;
 
+	RubixCube* testCube = new RubixCube();
 
-	/*
 	
-	while(true)   or for some number of iterations
+	
+	while(numIterations <MAX_ITERATIONS)  // or for some number of iterations
 
 	{
+	numIterations++;
+
+	if(startingCube->IsSolved())
+	{
+		std::cout << "Rubix Cube SOLVED!  It took:  " << numIterations << " iterations after an initial " << STARTING_TURNS << "." << std::endl;
+		break;
+	}
+
+
+	else
+	{
+		*testCube = *startingCube;
+		//Neighbor relationships:   rotate rows with some number of turns, rotate cols with some number fo turns, rotate BOTH with some number of turns
+		
+		int index = rand() % 3;
+
+
+		//Rotate rows
+
+		for (int i = 0; i < NUM_ROTATIONS_ROWS; i++)
+		{
+			testCube->TurnRow(index);
+		}
+/*
+		//Rotate cols
+		for (int i = 0; i < NUM_ROTATIONS_COLS; i++)
+		{
+			testCube->TurnColumn(index);
+		}
+
+		//Rotate rows & cols
+		for (int i = 0; i < NUM_ROTATIONS_BOTH; i++)
+		{
+			testCube->TurnColumn(index);
+			testCube->TurnRow(index);
+		
+		}
+
+		*/
+
+
+		//Fitness fns
+		
+		//Sides solved
+		if(testCube->SidesSolved() > startingCube->SidesSolved())
+		
+		//# rows & cols solved
+		//if((testCube->RowsFilled() + testCube->ColsFilled()) > (startingCube->ColsFilled() + startingCube->RowsFilled()))
+		
+		//# squares solved
+		//if (testCube->SquaresFilled() > startingCube->SquaresFilled())
+			{
+			*startingCube = *testCube;
+			}
+
+	
+	}
 
 
 
@@ -32,8 +96,8 @@ int main()
 	
 	
 	
+	return 0;
 	
-	*/
 
 
 
